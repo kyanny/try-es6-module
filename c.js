25d2521a2ffc8b1734dd8c20f29699a03742d40d
Kensuke Nagae <kyanny@gmail.com>
2015-08-11 15:47:59 +0900

    More example

---
+++ c.js
@@ -0,0 +1,11 @@
+export default class C {
+  constructor() {
+    console.log('C');
+  }
+}
+
+export class CC {
+  constructor() {
+    console.log('CC');
+  }
+}
--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import A from './a';
 import {
   B, BB
 } from './b';
+import C from './c';
+import {
+  CC
+} from './c';
 
 new A();
 new B();
 new BB();
+new C();
+new CC();
